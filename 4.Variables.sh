b91ddbfe4426b600f865c36e3c57f6ef8466d782
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-02 15:59:51 -0500

    actions

--- 4.Variables.sh
+++ 4.Variables.sh
@@ -2,9 +2,12 @@
 echo "Enter your user name: "
 read USER_NAME     # But still you can ur username while you are entering use '-s'
 
-echo "your user name is $USER_NAME "
+echo "Your username is $USER_NAME "
 
 echo "Enter your password: "
 read -s PASSWORD
+
+echo "Your password is $PASSWORD "
+
 
 
