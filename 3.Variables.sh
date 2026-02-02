7f16e9ba1a3b7d8cedd9e601792b3aa43dd033c5
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-02 15:49:19 -0500

    actions

---
+++ 3.Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello! $PERSON2, How are you?"
+echo "$PERSON2:: I'm doing great, How are you $PERSON1?"
+echo "$PERSON1:: Thanks for asking, Ima also good ?"
+echo "$PERSON2:: what are you doing today $PERSON1?"
