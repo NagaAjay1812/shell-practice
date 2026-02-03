0c4f1ece54f6c532d28591d29dcf6e0ece9d3029
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-02 21:41:46 -0500

    actions

--- 9.Installation.sh
+++ 9.Installation.sh
@@ -12,6 +12,6 @@ dnf install nginx -y
 if [ $? -ne 0]; then
     echo "Installation of Nginx.....Failure"
 else
-    echo "Installtion of Nginx....Success"
+    echo "Installation of Nginx....Success"
 fi
 
