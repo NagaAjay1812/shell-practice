f7bf2e2b8c82c640b86923b657c437806794bac8
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-02 20:40:55 -0500

    actions

--- 7.Variables.sh
+++ 7.Variables.sh
@@ -6,7 +6,7 @@ echo "Number of variables are passed to script: $# "
 echo "Present directory : $PWD"
 echo "Who is running : $USER"
 echo "Home directory :$HOME"
-echo "PID of script : $$ "
+echo "PID of script :$$"
 sleep 10 &
 echo background process id: $!"
 
