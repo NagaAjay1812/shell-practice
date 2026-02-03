d37d44fa88a0eb3c596523e08a7d344b3fc41f44
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-02 20:46:24 -0500

    actions

--- 8.Conditions.sh
+++ 8.Conditions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-NUMBER= 30
-if[$NUMBER -gt 20]; then
+NUMBER=$1
+if [ $NUMBER -gt 20 ]; then
     echo "given number: $NUMBER is greater than 20"
+else
+    echo "Given number: $NUMBER is not greather than 20"
 fi
