fa1859b0bdd1525c887c2a74d550f7f33a2a5716
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-03 11:05:44 -0500

    actions

---
+++ 11.Logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-practise"
+LOGS_FILE="/var/logs/shell-practise/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "please run the script with root user access." | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){ 
+    if [ $1 -ne 0 ]; then
+        echo "$2.....Failure" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2.....Success" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL"
