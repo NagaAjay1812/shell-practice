c2e6c271c49c4869777c28fee8ced5156c69c06a
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-02 21:45:00 -0500

    actions

--- 9.Installation.sh
+++ 9.Installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERID=$(id -u)
 
-if [$USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "run the script with root user access"
     exit 1
 fi
@@ -9,7 +9,7 @@ fi
 echo "Installing Nginx"
 dnf install nginxxxx -y
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Installation of Nginx.....Failure"
     exit 1
 else
