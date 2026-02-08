4826e17c692494e75f91b4934279bde3f8039cf4
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-08 12:44:49 -0500

    updated script

--- 17.Delete_old_logs.sh
+++ 17.Delete_old_logs.sh
@@ -27,6 +27,8 @@ FILES_TO_DELETE=$(find $LOGS_FOLDER -name "*.log" -type f -mtime -14)  #-14: mea
 while IFS= read -r line; do # seperate line by line and store in 'line' variable.
   # Process the line here
   echo "Deleting files: $line" # print the line
+  rm -f $line #deleted the files suceesfully
+  echo "deleted the files successfully"
 done <<< "$FILES_TO_DELETE" # to read input variable we use <<< ; to read file we use <.
 
 
