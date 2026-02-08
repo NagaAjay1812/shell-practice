50a090769dcce8265d57645c375d2eb1f168c1ab
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-08 15:31:31 -0500

    updated script

--- 19.Backups.sh
+++ 19.Backups.sh
@@ -2,6 +2,9 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/backup.log"
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+
 
 R="\e[31m"
 G="\e[32m"
@@ -22,6 +25,22 @@ USAGE(){
 if [ $# -lt 2 ]; then
     USAGE
     exit 1
+fi
+
+##directories exist or not
+if [! -d $SOURCE_DIR ]; then
+    echo -e "$R Source directory doesn't exist $N"
+    exit 1
+fi
+
+if [! -d $DESTINATION_DIR ]; then
+    echo -e "$R destination directory doesn't exist $N"
+    exit 1
+fi
+
+#find the files in source directory
+if [ find ./home/ec2-user/app-logs -name "*.log" -type f -mtime -14 ]; then
+    echo "displaying the files"
 fi
 
 
