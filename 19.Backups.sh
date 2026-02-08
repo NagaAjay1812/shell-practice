7809e132eb17c8b564809937cdc799ce87d0d84b
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-08 16:01:44 -0500

    updated script

--- 19.Backups.sh
+++ 19.Backups.sh
@@ -4,7 +4,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/backup.log"
 SOURCE_DIR=$1
 DESTINATION_DIR=$2
-
+DAYS={3:: +14}
 
 R="\e[31m"
 G="\e[32m"
@@ -22,6 +22,10 @@ USAGE(){
     exit 1
 }
 
+LOGS(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1 " | tee -a $LOG_FILE
+}
+
 if [ $# -lt 2 ]; then
     USAGE
     exit 1
@@ -29,15 +33,24 @@ fi
 
 ##directories exist or not
 if [! -d $SOURCE_DIR ]; then
-    echo -e "$R Source directory doesn't exist $N"
+    echo -e "$R Source directory : $SOURCE_DIR doesn't exist $N"
     exit 1
 fi
 
 if [! -d $DESTINATION_DIR ]; then
-    echo -e "$R destination directory doesn't exist $N"
+    echo -e "$R Destination directory :$DESTINATION_DIR doesn't exist $N"
     exit 1
 fi
 
 #find the files in source directory
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime -14 )
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS )
+
+log "Backup started"
+log "Source directory : $SOURCE_DIR "
+log "Destination directory : $DESTINATION_DIR "
+log "Days : $DAYS "
+
+if [ -z $FILES ]; then 
+    log "Files doesnt exist to archieve"
+fi
 
