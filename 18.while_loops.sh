f88e36f6ddfe0bcad9573a7720966ac867e4eb00
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-08 12:27:34 -0500

    updated script

--- 18.while_loops.sh
+++ 18.while_loops.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-count=1
-while [ count -le 5 ]
-do
-    echo "count is $count"
-    sleep 1 
-    ((count++)) #Increment counter
-done
+# count=1
+# while [ count -le 5 ]
+# do
+#     echo "count is $count"
+#     sleep 1 
+#     ((count++)) #Increment counter
+# done
+
+#!/bin/bash
+
+FILENAME="13.loops.sh"
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Process the line here
+  echo "Line content: $line"
+done < "$FILENAME"
